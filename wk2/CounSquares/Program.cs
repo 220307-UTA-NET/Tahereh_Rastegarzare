7eae2d55468cdb296e4ec00b9e1fec1869b6489a
Tahereh <tahereh.rastegarzare@gmail.com>
2022-06-23 15:15:54 -0700

    added week 2 codechallenge

---
+++ wk2/CounSquares/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+class Program{
+
+    public static int countSquares(int a, int b)
+    {
+        int count= (int)(Math.Sqrt(b)) - (int)(Math.Sqrt(a));  
+        if(a == (int)(Math.Sqrt(a))*(int)(Math.Sqrt(a)))  
+           count += 1; 
+        return count;
+
+    }
+     public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a lower bound");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the upperbound");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The number of square intergers between " + a +"and "+ b + " is "+ countSquares(a, b));
+    }
+
+}
